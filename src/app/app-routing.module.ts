e600579e897cc167087ce41e31a826bfbe27cbcb
rahhi <rahhi60@gmail.com>
2021-03-29 18:09:51 +0530

    added intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'intro', component: IntroComponent },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
